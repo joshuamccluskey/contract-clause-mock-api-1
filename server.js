265833ff774559a54f6979d81bbdb83022f51b6a
joshuamccluskey <jpiff57@gmail.com>
2022-03-27 13:09:25 -0700

    Refoactored out router 2

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router1 = jsonServer.router('clausesAbove35K.json');
-const router2 = jsonServer.router('clausesBelow35K.json')
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router1);
-server.use(router2)
 server.listen(port);
