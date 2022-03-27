c43cde8ef27c52fb3bc56508aa515725911635a1
joshuamccluskey <jpiff57@gmail.com>
2022-03-27 13:06:58 -0700

    Updated server to read two json files

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('clausesAbove35K.json');
+const router1 = jsonServer.router('clausesAbove35K.json');
+const router2 = jsonServer.router('clausesBelow35K.json')
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
-server.use(router);
+server.use(router1);
+server.use(router2)
 server.listen(port);
